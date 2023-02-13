f1d0c8529233995700be5d8d3343ba73b3b40784
kanykei063129 <akjoltoikyzykanykei@gmail.com>
2023-02-13 16:53:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import person.Builder;
+import person.Driver;
+import person.Programmer;
+
+public class Main {
+    public static void main(String[] args) {
+        Driver driver = new Driver("Khafiz",18);
+        System.out.println(driver.toString());
+        Programmer programmer = new Programmer("Kanykei",16);
+        System.out.println(programmer.toString());
+        Builder builder = new Builder("Madina",17);
+        System.out.println(builder.toString());
+    }
+}
